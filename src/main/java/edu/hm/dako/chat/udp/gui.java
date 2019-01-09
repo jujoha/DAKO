2d3222aa469d7572beaba1c4738d359e382a6b16
Julian <juleh1996@gmx.de>
2019-01-09 21:42:01 +0100

    gui fix

--- src/main/java/edu/hm/dako/chat/udp/gui.java
+++ src/main/java/edu/hm/dako/chat/udp/gui.java
@@ -3,21 +3,35 @@ package edu.hm.dako.chat.udp;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionListener;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.UnknownHostException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 import javax.swing.border.EmptyBorder;
+
+import edu.hm.dako.chat.common.AuditLogPDU;
+import edu.hm.dako.chat.server.SimpleChatServerImpl;
+
 import javax.swing.JButton;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 import javax.swing.Action;
+
 
 public class gui extends JFrame {
 
 	private JPanel contentPane;
 	private final Action tcpStarten = new SwingAction();
 	private final Action udpStarten = new SwingAction_1();
+	
+	
 
 	/**
 	 * Launch the application.
@@ -56,6 +70,8 @@ public class gui extends JFrame {
 		tcp.setAction(udpStarten);
 		tcp.setBounds(256, 85, 117, 81);
 		contentPane.add(tcp);
+		
+		
 	}
 
 	private class SwingAction extends AbstractAction {
@@ -64,31 +80,40 @@ public class gui extends JFrame {
 			putValue(SHORT_DESCRIPTION, "startet den TCP Server");
 		}
 		public void actionPerformed(ActionEvent e) {
-			try {
-				AuditLogTCP server = new AuditLogTCP();
-				server.run();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		
+			// just for not interrupting the GUI
+			new Thread(new Runnable() {
+			    public void run() {
+			    	try {
+			    		AuditLogTCP server = new AuditLogTCP();
+						server.run();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}).start();
 		}
-		
 	}
+	
 	private class SwingAction_1 extends AbstractAction {
 		public SwingAction_1() {
 			putValue(NAME, "udp Starten");
 			putValue(SHORT_DESCRIPTION, "Startet den udp server");
 		}
 		public void actionPerformed(ActionEvent b) {
-			try {
-				AuditLogServer server = new AuditLogServer();
-				server.run();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
+			// just for not interrupting the GUI
+			new Thread(new Runnable() {
+			    public void run() {
+			    	try {
+						AuditLogServer server = new AuditLogServer();
+						server.run();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}).start();
 		}
 	}
+	
 }
